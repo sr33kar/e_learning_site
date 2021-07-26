2f79558a679b64a24df76f1ae665615f37c4b368
Sreekar <gadasusreekar@gmail.com>
2021-07-26 21:16:53 +0530

    project creation

---
+++ eLearning_site/src/main/java/com/example/eLearning_site/ELearningSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.eLearning_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningSiteApplication.class, args);
+	}
+
+}
